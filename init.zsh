37e56fc4f9a66751f2321fe7f9d1b1b63de3e9e3
Philip M. Gollucci <pgollucci@p6m7g8.com>
2026-02-07 09:25:05 -0500

    chore: p6common is redundant

--- init.zsh
+++ init.zsh
@@ -8,7 +8,6 @@
 ######################################################################
 p6df::modules::playwright::deps() {
   ModuleDeps=(
-    p6m7g8-dotfiles/p6common
     p6m7g8-dotfiles/p6df-js
   )
 }
